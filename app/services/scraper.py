38f3a5c17e7de216c79bb48c3d137d37718bd019
Derek Nguyen <derek.nguyen02@gmail.com>
2023-08-09 22:22:33 -0700

    extracted zip links

--- app/services/scraper.py
+++ app/services/scraper.py
@@ -4,12 +4,28 @@ USE THIS FILE TO STORE ALL WEB CRAWLER FUNCTIONS
 
 import requests
 import pandas
+from bs4 import BeautifulSoup
 from zipfile import ZipFile
 
+zip_links = []
 
-url = "https://www.sec.gov/dera/data/crowdfunding-offerings-data-sets/files/dera/data/crowdfunding-offerings-data-sets/2023q2_cf.zip"
-req = requests.get(url)
+def get_zip_links(url: str) -> str:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-filename = url.split('/')[-1]
+    for link in soup.find_all("a"):
+        try:
+            if link.get("href").endswith(".zip") == True:
+                zip_links.append(link.get("href"))
+        except AttributeError:
+            print('Not a string')
+            
 
-print(filename)
+    # return zip_links
+
+
+# data = get_zip_links("https://www.sec.gov/dera/data/crowdfunding-offerings-data-sets")
+
+get_zip_links("https://www.sec.gov/dera/data/crowdfunding-offerings-data-sets")
+
+print(zip_links[0])
