ce00883ef67d58de1faa0ea8249d4b86e15a5026
Derek Nguyen <derek.nguyen02@gmail.com>
2023-11-08 19:06:58 -0800

    added db adapter for all companies

---
+++ app/databases/bigquery_adapters.py
@@ -0,0 +1,24 @@
+import json
+from typing import Any
+from google.cloud import bigquery
+from datetime import date
+
+client = bigquery.Client()
+
+def get_all_companies():
+    query = """
+    select * 
+    from graphy-financials.main.companies
+    """
+    
+    query_job = client.query(query)
+    results = query_job.result()
+    
+    companies_list = []
+    
+    for company in results:
+        company_dict = dict(company.items())
+        companies_list.append(company_dict)
+    
+    companies_json = json.dumps(company_dict, default=str)
+    return companies_json
--- app/databases/database.py
+++ app/databases/database.py
@@ -3,60 +3,52 @@ from google.cloud import bigquery
 
 conn = get_db_connection()
 
-# conn = psycopg2.connect(
-#     database="graphyfinancials",
-#     host="localhost",
-#     user="derek",
-#     password="",
-#     port="5432"
-# )
+# cursor = conn.cursor()
 
-cursor = conn.cursor()
-
-def createTables():
-    cursor.execute(
-        """
-        create table companies (
-            company_title varchar(255),
-            street_1 varchar(255),
-            street_2 varchar(255),
-            city varchar(255),
-            state_or_country varchar(255),
-            zipcode varchar(255),
-            website varchar(255),
-            cik varchar(255),
-            date_incorporation date
-        );
+# def createTables():
+#     cursor.execute(
+#         """
+#         create table companies (
+#             company_title varchar(255),
+#             street_1 varchar(255),
+#             street_2 varchar(255),
+#             city varchar(255),
+#             state_or_country varchar(255),
+#             zipcode varchar(255),
+#             website varchar(255),
+#             cik varchar(255),
+#             date_incorporation date
+#         );
         
-        create table financials (
-            cik varchar(255),
-            revenue float,
-            cogs float,
-            tax float,
-            total_assets float,
-            cash_equity float,
-            act_received float,
-            short_term_debt float,
-            long_term_debt float
-        )
-        """
-        )
+#         create table financials (
+#             cik varchar(255),
+#             revenue float,
+#             cogs float,
+#             tax float,
+#             total_assets float,
+#             cash_equity float,
+#             act_received float,
+#             short_term_debt float,
+#             long_term_debt float
+#         )
+#         """
+#         )
 
-def dropTables():
-    cursor.execute(
-        """
-        drop table if exists companies;
-        drop table if exists financials;
-        """
-        )
+# def dropTables():
+#     cursor.execute(
+#         """
+#         drop table if exists companies;
+#         drop table if exists financials;
+#         """
+#         )
     
-def buildLocalTables():
-    print('Dropping tables')
-    dropTables()
-    print('Finished dropping tables')
-    print('Building tables')
-    createTables()
-    print('Finished building tables')
+# def buildLocalTables():
+#     print('Dropping tables')
+#     dropTables()
+#     print('Finished dropping tables')
+#     print('Building tables')
+#     createTables()
+#     print('Finished building tables')
 
-buildLocalTables()
-conn.commit()
+# buildLocalTables()
+# conn.commit()
