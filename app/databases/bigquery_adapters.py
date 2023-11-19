6df170561f69c617420ba89ff888505bd8fd93e7
Derek Nguyen <derek.nguyen02@gmail.com>
2023-11-18 20:19:47 -0800

    completed api data endpoints

--- app/databases/bigquery_adapters.py
+++ app/databases/bigquery_adapters.py
@@ -20,5 +20,48 @@ async def get_all_companies():
         company_dict = dict(company.items())
         companies_list.append(company_dict)
     
-    companies_json = json.dumps(companies_list, default=str)
-    return companies_json
+    return companies_list
+
+async def get_company(cik: str):
+    query = f"""
+    select *
+    from graphy-financials.main.companies
+    where 1=1
+    and cik = '{cik}'
+    """
+    
+    query_job = client.query(query)
+    results = query_job.result()
+    
+    company_dict = None
+    for company in results:
+        try:
+            company_dict = dict(company)
+        except Exception as e:
+            print('Error converting to dictionary',e)
+    
+    if not company_dict:
+        return {'Error': 'No company found'}
+    else:
+        return company_dict
+    
+async def get_company_financials(cik: str):
+    query = f"""
+    select *
+    from graphy-financials.main.financials
+    where 1=1
+    and cik = '{cik}'
+    """
+    
+    query_job = client.query(query)
+    results = query_job.result()
+    
+    financials_list = []
+    
+    for financial in results:
+        financial_dict = dict(financial.items())
+        financials_list.append(financial_dict)
+    return financials_list
+
+# print(get_company_financials('1882217'))
+# print(get_company('1882217'))
--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI, APIRouter,HTTPException
-from databases.bigquery_adapters import get_all_companies
+from databases.bigquery_adapters import get_all_companies, get_company, get_company_financials
 
 app = FastAPI()
-
 company_router = APIRouter()
+company_financial_router = APIRouter()
 
 # Root route
 @app.get('/')
 async def root():
     return {'message': 'Hello World!'}
 
-# Subrouter route for /company
+# Subrouter route for /companies
 @company_router.get('/')
 async def get_all_company():
     # return {'message': 'List of companies'}
@@ -22,4 +22,22 @@ async def get_all_company():
         raise HTTPException(status_code=500)
     return companies
 
-app.include_router(company_router, prefix='/company')
+@company_router.get('/{cik_id}')
+async def get_company_info(cik_id):
+    try:
+        company = await get_company(f'{cik_id}')
+        return company
+    except Exception as e:
+        print('Error occurred while getting company details: ',e)
+
+
+@company_financial_router.get('/{cik_id}')
+async def get_financial_info(cik_id):
+    try:
+        financial_data = await get_company_financials(cik_id)
+        return financial_data
+    except Exception as e:
+        print('Error occurred while getting financial data: ',e)
+
+app.include_router(company_router, prefix='/companies')
+app.include_router(company_financial_router,prefix='/financials')
